99d4017ffa2da6f219f1e87ab8fd2e5e86e281eb
Rex Zulfekar <rex.zulfekar@viacom.com>
2024-05-02 14:39:06 -0400

    setup with iife

--- vite-starter/main.js
+++ vite-starter/main.js
@@ -1,13 +1,16 @@
 import './style.css';
 import { getVideo, drawVideo } from './src/camera';
 
-const video = await getVideo();
-const canvas = document.querySelector('canvas');
-const button = document.getElementById('camera');
 
-button.addEventListener('click', () => {
-  console.log('drawing');
-  drawVideo(video, canvas);
-});
 
-await video.play();
+
+(async () => {
+  const video = await getVideo();
+  const canvas = document.querySelector('canvas');
+  const button = document.getElementById('camera');
+  button.addEventListener('click', () => {
+    console.log('drawing');
+    drawVideo(video, canvas);
+  });
+})();
+
