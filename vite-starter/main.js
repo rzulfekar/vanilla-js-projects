93b78a0a5229fc914a81393da2f7f360e5fea0c5
Rex Zulfekar <rzulfekar@users.noreply.github.com>
2024-05-15 18:30:42 -0400

    some changes with prettier

--- vite-starter/main.js
+++ vite-starter/main.js
@@ -1,16 +1,12 @@
-import './style.css';
-import { getVideo, drawVideo } from './src/camera';
-
-
-
+import "./style.css";
+import { getVideo, drawVideo } from "./src/camera";
 
 (async () => {
   const video = await getVideo();
-  const canvas = document.querySelector('canvas');
-  const button = document.getElementById('camera');
-  button.addEventListener('click', () => {
-    console.log('drawing');
+  const canvas = document.querySelector("canvas");
+  const button = document.getElementById("camera");
+  button.addEventListener("click", () => {
+    console.log("drawing");
     drawVideo(video, canvas);
   });
 })();
-
--- vite-starter/src/camera.js
+++ vite-starter/src/camera.js
@@ -1,19 +1,19 @@
 export async function getVideo() {
-    const avStream = await navigator.mediaDevices.getUserMedia({
-        // audio: true,
-        video: true
-    });
+  const avStream = await navigator.mediaDevices.getUserMedia({
+    // audio: true,
+    video: true,
+  });
 
-    const video = document.createElement('video');
-    video.srcObject = avStream;
-    await video.play();
-    return video;
+  const video = document.createElement("video");
+  video.srcObject = avStream;
+  await video.play();
+  return video;
 }
 
 export function drawVideo(video, canvas) {
-    const context = canvas.getContext("2d");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    console.log(video.videoWidth, video.videoHeight);
-    context.drawImage(video, 0, 0);
+  const context = canvas.getContext("2d");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  console.log(video.videoWidth, video.videoHeight);
+  context.drawImage(video, 0, 0);
 }
